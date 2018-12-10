6a2f57416938596ba395e246cf1f8b05887bc145
kck15 <kck15911@gmail.com>
2018-12-09 19:09:57 -0600

    git init

---
+++ app/src/main/java/com/kck15911gmail/randominizer/MainActivity.kt
@@ -0,0 +1,25 @@
+package com.kck15911gmail.randominizer
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Button
+import android.widget.SeekBar
+import android.widget.TextView
+import java.util.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        val rollButton = findViewById<Button>(R.id.rollButton)
+        val resultsTextView = findViewById<TextView>(R.id.resultsTextView)
+        val seekBar = findViewById<SeekBar>(R.id.seekBar)
+
+        rollButton.setOnClickListener {
+            val rand = Random().nextInt(seekBar.progress)
+            resultsTextView.text = rand.toString()
+        }
+    }
+}
